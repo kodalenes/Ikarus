84179aa9fbd44ddedd5daffc02eb2af467a633bd
Ahmet Enes Kodal <kodalenes.900@gmail.com>
2026-04-22 17:45:01 +0300

    add wolksavegen funk bg music 😄

--- includes/footer.php
+++ includes/footer.php
@@ -4,7 +4,34 @@
         <span class="footer-logo">IKARUS</span>
         <p class="footer-text">&copy; 2026 Ikarus Tournament Platform. All rights reserved.</p>
     </div>
- </footer>
+
+<div class="music-control-wrapper">
+    <audio id="bgMusic" loop>
+        <source src="../assets/images/VOLKSWAGEN FUNK.ogg" type="audio/mpeg">
+    </audio>
+    
+    <button id="musicToggle" class="music-btn" title="Müziği Başlat/Durdur">
+        <span id="musicStatusIcon">🎵</span>
+    </button>
+</div>
+
+<script>
+    const music = document.getElementById('bgMusic');
+    const btn = document.getElementById('musicToggle');
+    const icon = document.getElementById('musicStatusIcon');
+
+    btn.addEventListener('click', () => {
+        if (music.paused) {
+            music.play();
+            btn.classList.add('playing');
+            icon.innerText = "⏸️";
+        } else {
+            music.pause();
+            btn.classList.remove('playing');
+            icon.innerText = "🎵";
+        }
+    });
+</script>
 
  <script src="../assets/js/FormValidator.js"></script>
  <script src="../assets/js/utils.js"></script>
